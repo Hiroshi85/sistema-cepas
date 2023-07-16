ba3bd65ddd9e600179d51e0915cf7c9b26e35a67
Hiroshi <ebruno.hiroshi@gmail.com>
2023-07-16 01:13:18 -0500

    add: asistencia_asignatura migration

---
+++ database/migrations/2023_07_16_010611_create_asistencia_asignatura_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('asistencia_asignatura', function (Blueprint $table) {
+            $table->id("idasistencia");
+            $table->unsignedBigInteger("idalumno");
+            $table->unsignedBigInteger("idcurso");
+            $table->integer("bimestre");
+            $table->char("s1",1)->default('F');
+            $table->char("s2",1)->default('F');
+            $table->char("s3",1)->default('F');
+            $table->char("s4",1)->default('F');
+            $table->char("s5",1)->default('F');
+            $table->char("s6",1)->default('F');
+            $table->char("s7",1)->default('F');
+            $table->char("s8",1)->default('F');
+            
+            $table->foreign('idcurso')->references('id')->on('asignaturas');
+            $table->foreign('idalumno')->references('idalumno')->on('alumnos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('asistencia_asignatura');
+    }
+};
