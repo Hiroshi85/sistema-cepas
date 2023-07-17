aa2b920c0b3f4f7634f05323cc3c260cdc76d562
JohannCastillo <johannjco15022@gmail.com>
2023-07-17 17:56:53 -0500

    dni nullable en tabla usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('dni');
+            $table->string('dni')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
