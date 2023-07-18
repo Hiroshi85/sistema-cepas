d725cb0b8af64fc8972b1abd25dc62d87be377fe
brunomont <brunoadrian.mont@gmail.com>
2023-07-18 03:34:05 -0500

    mod: postulacion table before ofertas

--- database/migrations/2023_06_19_011000_create_ofertas_table.php
+++ database/migrations/2023_06_19_011000_create_ofertas_table.php
@@ -13,15 +13,16 @@ class CreateOfertasTable extends Migration
     {
         Schema::create('ofertas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('puesto_id');
             $table->date('fecha_inicio');
             $table->date('fecha_fin')->nullable();
             $table->text('descripcion');
             $table->decimal('salario', 10, 2);
             $table->text('beneficios')->nullable();
+            $table->enum('estado', ['aceptada', 'rechazada', 'pendiente'])->default('Pendiente');
             $table->timestamps();
 
-            $table->foreign('puesto_id')->references('id')->on('puestos')->onDelete('cascade');
+            $table->unsignedBigInteger('postulacion_id');
+            $table->foreign('postulacion_id')->references('id')->on('postulaciones')->onDelete('cascade');
         });
     }
 
