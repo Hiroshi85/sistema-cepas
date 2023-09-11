304b9b0ba2be1ac5bfa10099981943823887876c
Hiroshi <ebruno.hiroshi@gmail.com>
2023-09-11 18:45:59 -0500

    add: buscar alumno modal componente

--- resources/views/comportamiento/show.blade.php
+++ resources/views/comportamiento/show.blade.php
@@ -13,7 +13,7 @@
                     <a class="text-gray-800 dark:text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow hover:bg-gray-200 transition duration-300 ease-in-out"
                     href="{{ route('comportamientos.index') }}">
                         Atrás
-                    </a>                    
+                    </a>
                 </div>
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-1">
@@ -27,14 +27,6 @@
                                     <label for="alumno" class="block">Alumno: </label>
                                     <input type="text" required readonly id="alumno_s" name="alumno_s" class="w-full">
                                 </div>
-                                {{-- <div>
-                                    <label for="alumno" class="block">Alumno: </label>
-                                    <select required id="alumno" name="alumno" class="w-full">
-                                    @foreach ($alumnos as $al)
-                                        <option value="{{$al->id}}">{{$al->nombres}} {{$al->apellidos}}</option>  
-                                    @endforeach
-                                    </select>
-                                </div> --}}
                                 <div>
                                     <label for="bimestre" class="block">Bimestre: </label>
                                     <select required id="bimestre" name="bimestre" class="w-full dark:text-gray-800">
@@ -51,7 +43,7 @@
                                     </button>
                                 </div>
                             </div>
-                        </form>                      
+                        </form>
                     </div>
             </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-5">
@@ -77,41 +69,16 @@
                                 </tr>
                             </thead>
                             <tbody class="dark:bg-gray-800 divide-y divide-gray-700 dark:bg-gray-900 dark:divide-gray-500">
-                                
+
                             </tbody>
                         </table>
-                    </div>                    
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div id="buscarAlumnoModal" class="fixed z-10 inset-0 overflow-y-auto hidden w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-        <div class="modal-container bg-white w-full max-w-3xl mx-auto rounded shadow-lg z-50 overflow-y-auto p-4">
-            <div class="modal-content py-4 px-6">
-              <div class="flex justify-between items-center mb-4">
-                <h2 class="text-xl font-semibold">Buscar Alumno</h2>
-                <button class="modal-close cursor-pointer z-50" onclick="closeModal()">&times;</button>
-              </div>
-              <div class="mb-4 grid grid-cols-6 gap-1">
-                <input type="text" id="searchInput" class="col-span-5 border border-gray-400 rounded w-full px-3 py-2" placeholder="Buscar alumno...">
-                <button id="searchButton" id="searchButton" class="col-span-1 bg-blue-500 text-white px-4 py-2 rounded ml-2">Buscar</button>
-              </div>
-              <table class="w-full">
-                <thead>
-                  <tr>
-                    <th class="px-4 py-2">Nombres y apellidos</th>
-                    <th class="px-4 py-2">Opción</th>
-                  </tr>
-                </thead>
-                <tbody id="tableBody" class="divide-y divide-gray-700">
-                  <!-- Aquí se llenarán los datos de la tabla mediante JavaScript -->
-                </tbody>
-              </table>
-            </div>
-          </div>
-    </div>
-    
+    <x-modal.buscar-alumno id="buscarAlumnoModal" inputTrigger="alumno_s"/>
 </x-app-layout>
 <script>
   const alumnoHiddenIdInput = document.getElementById('alumno');
@@ -128,7 +95,7 @@
             console.log(error);
         });
   }
-  
+
   function buscarDatos() {
     const alumnoId = alumnoHiddenIdInput.value;
     const bimestreValue = bimestre.value;
@@ -167,7 +134,7 @@
         });
         nota.innerHTML = `Nota conductual: ${data.nota}`;
 
-        
+
         })
         .catch(error => {
             console.log(error);
@@ -179,80 +146,6 @@
   buscarButton.addEventListener('click', buscarDatos);
 </script>
 
-<script>
-        const buscarModalButton = document.getElementById("searchButton");
-        const buscarInput = document.getElementById("searchInput");
-        const alumnoInputOg = document.getElementById("alumno_s");
-        const hiddenAlumno = document.getElementById("alumno");
-        // Función para abrir el modal
-        function openModal() {
-            document.getElementById("buscarAlumnoModal").classList.remove("hidden");
-        }
-    
-        // Función para cerrar el modal
-        function closeModal() {
-            document.getElementById("buscarAlumnoModal").classList.add("hidden");
-        }
-    
-        // Función para llenar la tabla con los resultados de búsqueda
-        function fillTable(data) {
-            const tableBody = document.getElementById("tableBody");
-            tableBody.innerHTML = ""; // Limpiamos la tabla antes de llenarla
-    
-            data.forEach((alumno) => {
-            const row = document.createElement("tr");
-            const nombreApellidoCell = document.createElement("td");
-            const opcionCell = document.createElement("td");
-            const selectButton = document.createElement("p");
-    
-            nombreApellidoCell.textContent = alumno.nombre_apellidos;
-            nombreApellidoCell.className = "p-1 whitespace-nowrap text-center";
-            
+@stack('script')
 
-            selectButton.textContent = "Seleccionar";
-            selectButton.className = "cursor-pointer text-center p-1 whitespace-nowrap text-blue-600 dark:text-blue-500 hover:underline";
-            selectButton.addEventListener("click", () => {
-                hiddenAlumno.value = alumno.idalumno;
-                alumnoInputOg.value = alumno.nombre_apellidos;
-                closeModal();
-            });
-    
-            opcionCell.appendChild(selectButton);
-            row.appendChild(nombreApellidoCell);
-            row.appendChild(opcionCell);
-    
-            tableBody.appendChild(row);
-            });
-        }
-        
-        var searchResults;
-        // Evento click del botón "Buscar"
-        buscarModalButton.addEventListener("click", () => {
-            fetch("{{ route('alumn.buscar') }}?alumno="+buscarInput.value)
-            .then(response => response.json())
-            .then(data => {
-                searchResults = data["alumnos"];
-                console.log(searchResults);
-                fillTable(searchResults);
-            })
-            // Aquí realizarías la búsqueda de datos según el término de búsqueda
-            // y obtendrías un array de objetos con la estructura { nombreApellido: "Nombre Apellido" }
-            // Por ejemplo, para este ejemplo, se simula una búsqueda vacía:
-            
-            
-        });
-    
-        alumnoInputOg.addEventListener("click", (event)=>{
-            openModal();
-        })
-
-        window.addEventListener("click", function(event) {
-            if (event.target === document.getElementById("buscarAlumnoModal")) {
-                closeModal();
-            }
-        });
-
-      // Exponer la función de abrir el modal globalmente
-    //   window.openModal = openModal;
-</script>
 
