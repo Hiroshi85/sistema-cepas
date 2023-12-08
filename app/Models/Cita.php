88342cf5ef9e3721c8fb89e337234a247447a865
Hiroshi <ebruno.hiroshi@gmail.com>
2023-12-07 19:13:28 -0500

    add: procedimientos y ruta para citas en dashboard

--- app/Models/Cita.php
+++ app/Models/Cita.php
@@ -78,4 +78,43 @@ class Cita extends Model
     public function getFechaHoraFinAttribute($date){
         return Carbon::parse($date);
     }
+
+    public static function getCitasHoy(string $idUser){
+        $citas = Cita::join('alumnos as al', 'citas.alumno_id', '=', 'al.idalumno')
+        ->join('apoderados as ap', 'citas.apoderado_id', 'ap.idapoderado')
+        ->join('users as u', 'citas.citador_id', 'u.id')
+        ->join('aulas as a', 'al.idaula', 'a.idaula')
+        ->select('citas.id', 'alumno_id', 'apoderado_id', 'u.id as citador_id', 'al.nombre_apellidos as alumno', 'u.name as citador', 'ap.nombre_apellidos as apoderado','ap.numero_celular','a.grado','a.seccion','motivo', 'citas.estado', 'fechaHoraInicio', 'fechaHoraFin', 'duracionMinutos')
+        ->where('citas.estado', 'programado')
+        ->whereDate('fechaHoraInicio', Carbon::today())
+        ->orderBy('fechaHoraInicio', 'asc');
+
+        if($idUser){
+            $citas = $citas->where('citas.citador_id', $idUser);
+        }
+
+        return $citas->get();
+    }
+
+    public static function getCitasSemana(string $idUser){
+        $ultimoDia = Carbon::now()->endOfWeek();
+        $manhana = Carbon::tomorrow();
+
+        $citas = Cita::join('alumnos as al', 'citas.alumno_id', '=', 'al.idalumno')
+        ->join('apoderados as ap', 'citas.apoderado_id', 'ap.idapoderado')
+        ->join('users as u', 'citas.citador_id', 'u.id')
+        ->join('aulas as a', 'al.idaula', 'a.idaula')
+        ->select('citas.id', 'alumno_id', 'apoderado_id', 'u.id as citador_id', 'al.nombre_apellidos as alumno', 'u.name as citador', 'ap.nombre_apellidos as apoderado','ap.numero_celular','a.grado','a.seccion','motivo', 'citas.estado', 'fechaHoraInicio', 'fechaHoraFin', 'duracionMinutos')
+        ->whereBetween('fechaHoraInicio', [$manhana, $ultimoDia])
+        ->where('citas.estado', 'programado')
+        ->orderBy('fechaHoraInicio', 'asc');
+
+        if($idUser){
+            $citas = $citas->where('citas.citador_id', $idUser);
+        }
+
+        return $citas->get();
+    }
+
+
 }
