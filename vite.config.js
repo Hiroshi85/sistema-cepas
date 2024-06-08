49e8d8f6f4475f13632bc20df6be72ad0a8ebf78
Parms221 <willy_g29@outlook.com>
2024-06-08 11:28:31 -0500

    fix build npm

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/academia/current-cicle-card.css'
             ],
             refresh: true,
         }),
