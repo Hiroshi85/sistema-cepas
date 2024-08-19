ca79bdf309054f36f71e3bcc319d7c762c184f1c
Hiroshi <ebruno.hiroshi@gmail.com>
2024-08-19 18:27:35 -0500

    feat: descargar de gcp

--- app/Http/Controllers/PruebaArchivoController.php
+++ app/Http/Controllers/PruebaArchivoController.php
@@ -12,6 +12,12 @@ class PruebaArchivoController extends Controller
     {
         $pp = PruebaPsicologica::find($id);
         if(!is_null($pp->file_url)){
+            if(str_starts_with($pp->file_url, "psicologia/")){
+                $disk = Storage::disk('gcs');
+                $tempUrl = $disk->temporaryUrl($pp->file_url, now()->addMinutes(30));
+                error_log($tempUrl);
+                return redirect($tempUrl);
+            }
             return Storage::download($pp->file_url);
         }
         return abort(404);
